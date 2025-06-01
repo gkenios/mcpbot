ba2ea15f0318a590f3bd0c04cf81e1667769104b
George Gkenios <ggkenios@hotmail.com>
2025-06-02 00:19:45 +0200

    Move Host URL to env variable

--- mcpbot/shared/config/main.py
+++ mcpbot/shared/config/main.py
@@ -7,16 +7,9 @@ load_dotenv()
 ENV = os.getenv("ENV", "dev")
 LOCAL = os.getenv("LOCAL", "true").lower() != "false"
 PORT = os.getenv("PORT", 8000)
+HOST_URL = os.getenv("HOST_URL") if not LOCAL else "http://localhost:8000"
 
-COMPANY = "Devoteam"
 CONFIG_FILE = "mcpbot/config-local.yml" if LOCAL else "mcpbot/config-azure.yml"
+COMPANY = "Devoteam"
 
 CORS_ORIGINS = ["*"]
-
-if not LOCAL:
-    if ENV == "dev":
-        HOST_URL = "https://dev.devoteam.nl/mcpbot"
-    elif ENV == "prod":
-        HOST_URL = "https://prod.devoteam.nl/mcpbot"
-else:
-    HOST_URL = "http://localhost:8000"
